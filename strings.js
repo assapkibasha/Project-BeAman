3c6e46e5616c6b1edd669f7f9ace3329e224c8bf
ntsinzifrancois <assapkibasha@gmail.com>
2025-10-31 13:05:38 +0200

    js Strings and type coercion

---
+++ strings.js
@@ -0,0 +1,14 @@
+//check the type
+// function typeCheck(data){
+//   return typeof(data)
+// }
+// console.log(typeCheck('hello'))
+// console.log(typeCheck(100))
+
+//now let go to concatination
+
+let a = "hello" + "hello"
+console.log(a)
+
+let b = 'hello' +3//type coercion (automatic type conversion)
+console.log(b)
