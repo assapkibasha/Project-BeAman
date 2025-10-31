819cd3f174a341ce8f6b69db5a750c26bfee6e8b
ntsinzifrancois <assapkibasha@gmail.com>
2025-10-31 13:34:48 +0200

    kibs

--- customRoundLogic.js
+++ customRoundLogic.js
@@ -1,9 +1,13 @@
-function customRound(number){
-  if(number - Math.round(number)<0.5){//if number - number rounded is less than 0.5 round downward
-    return Math.ceil(number);
-  }else{
-    return Math.floor(number);//if number - rounded number is greater than 0.5 round upword
-  }
-}
-console.log(customRound(2.2));
-console.log(customRound(2.));
+// function customRound(number){
+//   if(number - Math.round(number)<0.5){//if number - number rounded is less than 0.5 round downward
+//     return Math.ceil(number);
+//   }else{
+//     return Math.floor(number);//if number - rounded number is greater than 0.5 round upword
+//   }
+// }
+// console.log(customRound(2.2));
+// console.log(customRound(2.));
+
+//another example
+
+
--- strings.js
+++ strings.js
@@ -1,27 +1,38 @@
-//check the type
- function typeCheck(data){
-   return typeof(data)
- }
-console.log(typeCheck('hello'))
-console.log(typeCheck(100))
+// //check the type
+//  function typeCheck(data){
+//    return typeof(data)
+//  }
+// console.log(typeCheck('hello'))
+// console.log(typeCheck(100))
 
-//now let go to concatination
+// //now let go to concatination
 
- let a = "hello" + "hello"
- console.log(a)
+//  let a = "hello" + "hello"
+//  console.log(a)
 
- let b = 'hello' +3//type coercion (auto;matic type conversion)
-console.log(b)
-;
-//math in string
-//javascript table of priority;
-// (...)
-// */;
-// +-
-;
+//  let b = 'hello' +3//type coercion (auto;matic type conversion)
+// console.log(b)
+// ;
+// //math in string
+// //javascript table of priority;
+// // (...)
+// // */;
+// // +-
+// ;
 
-console.log('$'+ 20.94 + 7.44);//not round;ed and not using priority table
-//string also can follow order of operation in javascript
-console.log('$'+ Math.round(20.94 + 7.44));;//rounded and using priority table
-//when counting money we need true value not rounded
-console.log('$'+ (2094 + 744)/100 +"  accurate value");//rounded and using priority table ie:removed point and we will later convert to cents again;;
+// console.log('$'+ 20.94 + 7.44);//not round;ed and not using priority table
+// //string also can follow order of operation in javascript
+// console.log('$'+ Math.round(20.94 + 7.44));;//rounded and using priority table
+// //when counting money we need true value not rounded
+// console.log('$'+ (2094 + 744)/100 +"  accurate value");//rounded and using priority table ie:removed point and we will later convert to cents again;;
+
+
+// items(2):                 $28.94
+//to do this use this way
+
+
+// let i = 'items('+ 1+1 +')';//items(11) but to solve this we will use
+
+
+let j = 'items('+ (1+1) +'):               '+ '$' + (2095 + 799)/100;
+console.log(j);
